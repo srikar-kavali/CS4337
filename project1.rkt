692e0a1958e0b5ec126f842dd0006a39c2b3a098
Srikar Kavali <srikar.kavali@gmail.com>
2025-10-23 18:35:46 -0500

    Added mode detection code from mode.rkt to detect interactive or batch mode

--- project1.rkt
+++ project1.rkt
@@ -1,2 +1,11 @@
 #lang racket
 
+(define prompt?
+   (let [(args (current-command-line-arguments))]
+     (cond
+       [(= (vector-length args) 0) #t]
+       [(string=? (vector-ref args 0) "-b") #f]
+       [(string=? (vector-ref args 0) "--batch") #f]
+       [else #t])))
+
+       
